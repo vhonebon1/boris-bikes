417e99a4967806377ccb0db10ce1f9bdd75d9ee6
Verity <vhonebon@gmail.com>
2017-06-06 22:52:31 +0100

    Adding dock function

--- lib/app.rb
+++ lib/app.rb
@@ -1,13 +1,21 @@
 class DockingStation
+  attr_reader :bike
+  
   def initialize
   end
 
   def release_bike
   	Bike.new
   end
+
+  def dock(bike)
+    @bike = bike
+  end
+
 end
 
 class Bike
+
   def initialize
   end
   
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -8,4 +8,15 @@ RSpec.describe DockingStation do
   it "should release a bike when the release_bike method is called" do
     expect((DockingStation.new).release_bike).to be_a Bike
   end
+
+  it "docks something" do
+  	bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it "returns docked bikes" do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
 end
